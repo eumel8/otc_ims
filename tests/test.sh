8354043220168fd5c267a804b88a0dd4ae669107
Frank Kloeker <f.kloeker@telekom.de>
2019-03-22 20:47:54 +0100

    update image name

--- tests/test.sh
+++ tests/test.sh
@@ -2,9 +2,9 @@
 
 # Prerequisites:
 # Cloud API Credentials
-# image disk on OBS ansible01:bionic-server-cloudimg-amd64-20180826.vmdk
+# image disk on OBS ansible011:bionic-server-cloudimg-amd64.vmdk
 echo "SCENARIO 1: create image"
-ansible-playbook test.yml -e "image_name=ansible-image01 image_url=ansible01:bionic-server-cloudimg-amd64-20180826.vmdk image_min_disk=4 localaction=create waitfor=true" || exit 1
+ansible-playbook test.yml -e "image_name=ansible-image01 image_url=ansible011:bionic-server-cloudimg-amd64.vmdk image_min_disk=4 localaction=create waitfor=true" || exit 1
 echo "SCENARIO 2: show image"
 ansible-playbook test.yml -e "image_name=ansible-image01 localaction=show" || exit 1
 echo "SCENARIO 3: list images (pagination)"
